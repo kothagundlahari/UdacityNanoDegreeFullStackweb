7017684ef419bf44f387c3802d79cda31fa59aa4
hari krisha <hari.k.krishna4@gmail.com>
2017-02-11 19:31:22 -0600

    added max width for the classes image and description

--- portfolio/toplist/style.js
+++ portfolio/toplist/style.js
@@ -1,4 +1,19 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  '*': {
+    'outline': '1px solid red !important'
+  },
+  '*': {
+    'WebkitBoxSizing': 'border-box',
+    'MozBoxSizing': 'border-box',
+    'MsBoxSizing': 'border-box',
+    'boxSizing': 'border-box'
+  },
+  'image': {
+    'maxWidth': [{ 'unit': 'px', 'value': 460 }]
+  },
+  'description': {
+    'maxWidth': [{ 'unit': 'px', 'value': 705 }]
+  }
 });

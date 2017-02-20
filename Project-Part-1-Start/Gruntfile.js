ef849259e6cefba5e2409d4ddeb7e40f3a77929d
hari krisha <hari.k.krishna4@gmail.com>
2017-02-20 17:58:10 -0600

    I removed smiley face and flower

--- Project-Part-1-Start/Gruntfile.js
+++ Project-Part-1-Start/Gruntfile.js
@@ -8,67 +8,78 @@
 
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    responsive_images: {
-      dev: {
-        options: {
-          engine: 'im',
-          sizes: [{
-            /*
-            Change these:
-            
-            width: ,
-            suffix: ,
-            quality:
-            */
-          }]
+    grunt.initConfig({
+        responsive_images: {
+            dev: {
+                options: {
+                    engine: 'im',
+                    sizes: [{ name: 'small', width: 320 }, { name: 'medium', width: 640 }, { name: 'large', width: 800 }]
+                },
+
+                /*
+                You don't need to change this part if you don't change
+                the directory structure.
+                */
+                files: [{
+                    expand: true,
+                    src: ['*.{gif,jpg,png}'],
+                    cwd: 'images_src/',
+                    dest: 'images/'
+                }]
+            }
         },
 
-        /*
-        You don't need to change this part if you don't change
-        the directory structure.
-        */
-        files: [{
-          expand: true,
-          src: ['*.{gif,jpg,png}'],
-          cwd: 'images_src/',
-          dest: 'images/'
-        }]
-      }
-    },
-
-    /* Clear out the images directory if it exists */
-    clean: {
-      dev: {
-        src: ['images'],
-      },
-    },
-
-    /* Generate the images directory if it is missing */
-    mkdir: {
-      dev: {
-        options: {
-          create: ['images']
+        /* Clear out the images directory if it exists */
+        clean: {
+            dev: {
+                src: ['images'],
+            },
         },
-      },
-    },
 
-    /* Copy the "fixed" images that don't go through processing into the images/directory */
-    copy: {
-      dev: {
-        files: [{
-          expand: true,
-          src: 'images_src/fixed/*.{gif,jpg,png}',
-          dest: 'images/'
-        }]
-      },
-    },
-  });
-  
-  grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+        /* Generate the images directory if it is missing */
+        mkdir: {
+            dev: {
+                options: {
+                    create: ['images']
+                },
+            },
+        },
+
+        /* Copy the "fixed" images that don't go through processing into the images/directory */
+        copy: {
+            dev: {
+                files: [{
+                    expand: true,
+                    src: 'images_src/fixed/*.{gif,jpg,png}',
+                    dest: 'images/'
+                }]
+            },
+        },
+
+        /* This is the configuration for image min */
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+
+                    src: 'images_src/fixed/*.{gif,jpg,png}',
+                    dest: 'images/'
+
+                }]
+            }
+        }
+
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+
+    grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images', 'imagemin']);
 
 };
--- Project-Part-1-Start/css/main.js
+++ Project-Part-1-Start/css/main.js
@@ -11,12 +11,20 @@ Hint! You'll probably want to start by styling:
 
 I've left some boilerplate below for you.
   'body': {
+    'width': [{ 'unit': '%H', 'value': 0.8 }],
+    'height': [{ 'unit': 'string', 'value': 'auto' }]
   },
   'article': {
     'width': [{ 'unit': 'em', 'value': 50 }]
   },
   'h1': {
+    'textAlign': 'center'
   },
   'img': {
+    'maxWidth': [{ 'unit': '%H', 'value': 1 }],
+    'height': [{ 'unit': 'string', 'value': 'auto' }]
+  },
+  'hr': {
+    'maxWidth': [{ 'unit': '%H', 'value': 0.8 }]
   }
 });

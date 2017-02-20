78c080e81b2efa8063e1b7d7c3d5dc1de319d5f6
hari krisha <hari.k.krishna4@gmail.com>
2017-02-20 12:31:26 -0600

    added the width 50em to the article

---
+++ Project-Part-1-Start/css/main.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  // Your styles go here!
+
+Hint! You'll probably want to start by styling:
+  * <body>
+  * <article>
+  * <h1>
+  * <img>
+
+I've left some boilerplate below for you.
+  'body': {
+  },
+  'article': {
+    'width': [{ 'unit': 'em', 'value': 50 }]
+  },
+  'h1': {
+  },
+  'img': {
+  }
+});

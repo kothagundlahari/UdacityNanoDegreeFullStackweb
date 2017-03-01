6c798d3e3b77cddd8d09959ae7f11fff93036bee
hari krisha <hari.k.krishna4@gmail.com>
2017-03-01 13:52:35 -0600

    give

--- puppyportfolio/css/style.js
+++ puppyportfolio/css/style.js
@@ -22,6 +22,9 @@ export default StyleSheet.create({
     'marginLeft': [{ 'unit': 'string', 'value': 'auto' }],
     'marginRight': [{ 'unit': 'string', 'value': 'auto' }]
   },
+  'hdr': {
+    'display': 'block'
+  },
   'text-muted': {
     'color': '#BCBBBB'
   },
@@ -39,41 +42,5 @@ export default StyleSheet.create({
     'color': 'dimgrey !important',
     'border': [{ 'unit': 'string', 'value': 'solid' }, { 'unit': 'px', 'value': 0.5 }, { 'unit': 'string', 'value': 'dimgrey' }, { 'unit': 'string', 'value': '!important' }],
     'height': [{ 'unit': 'px', 'value': 5 }, { 'unit': 'string', 'value': '!important' }]
-  },
-  // *
-*  Footer Styles
-*
-*
-*
-  '[class*="zocial-"]:before': {
-    'color': 'black',
-    'display': 'inline-block',
-    'fontFamily': ''zocial', sans-serif',
-    'textShadow': [{ 'unit': 'px', 'value': 3 }, { 'unit': 'px', 'value': 3 }, { 'unit': 'px', 'value': 3 }, { 'unit': 'string', 'value': '#aaa' }],
-    'width': [{ 'unit': 'em', 'value': 2 }]
-  },
-  'a:hover [class*="zocial-"]:before': {
-    'color': '#3d85c6'
-  },
-  'a': {
-    'color': '#4183C4',
-    'fontWeight': '300',
-    'textDecoration': 'none'
-  },
-  'a:hover': {
-    'color': '#3d85c6',
-    'textDecoration': 'underline'
-  },
-  'a#viewSource': {
-    'borderTop': [{ 'unit': 'px', 'value': 1 }, { 'unit': 'string', 'value': 'solid' }, { 'unit': 'string', 'value': '#ddd' }],
-    'display': 'block',
-    'margin': [{ 'unit': 'em', 'value': 1.3 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
-    'padding': [{ 'unit': 'em', 'value': 1 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
-  },
-  'div#social a': {
-    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'em', 'value': 1 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
-  },
-  'div#social a:last-of-type': {
-    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }]
   }
 });

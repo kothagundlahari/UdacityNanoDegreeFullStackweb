592ffa36d381d11fd86a0a08e60956723789bbc6
hari krisha <hari.k.krishna4@gmail.com>
2017-03-02 16:10:39 -0600

    trying to add flex box

--- puppyportfolio/css/responsive.js
+++ puppyportfolio/css/responsive.js
@@ -1,4 +1,20 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  'header_inner': {
+    'display': 'flex',
+    'flexWrap': 'wrap'
+  },
+  'header_image': {
+    'width': [{ 'unit': '%H', 'value': 0.2 }]
+  },
+  'header_text': {
+    'width': [{ 'unit': '%H', 'value': 0.8 }]
+  },
+  'header_image:nth-of-type(1)': {
+    'width': [{ 'unit': '%H', 'value': 0.2 }]
+  },
+  'header_image:nth-of-type(2)': {
+    'width': [{ 'unit': '%H', 'value': 0.8 }]
+  }
 });

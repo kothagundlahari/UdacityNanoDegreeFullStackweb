6700a9dfc6df8cd161e5fc0dbf300718d2ff42d8
hari krisha <hari.k.krishna4@gmail.com>
2017-03-03 10:38:31 -0600

    trying to change flex

--- puppyportfolio/css/responsive.js
+++ puppyportfolio/css/responsive.js
@@ -5,18 +5,9 @@ export default StyleSheet.create({
     'display': 'flex'
   },
   'header_image': {
-    'width': [{ 'unit': 'px', 'value': 100 }],
-    'width': [{ 'unit': '%H', 'value': 0.2 }],
-    'order': '0'
+    'width': [{ 'unit': 'px', 'value': 100 }]
   },
   'header_text': {
-    'width': [{ 'unit': '%H', 'value': 0.8 }],
-    'order': '1'
-  },
-  'header_image div:nth-of-type(1)': {
-    'width': [{ 'unit': '%H', 'value': 0.2 }]
-  },
-  'header_image div:nth-of-type(2)': {
     'width': [{ 'unit': '%H', 'value': 0.8 }]
   }
 });

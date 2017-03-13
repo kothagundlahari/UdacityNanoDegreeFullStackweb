2127076394c2bdfce566d41ad252335b953fc8aa
hari krisha <hari.k.krishna4@gmail.com>
2017-03-13 11:18:30 -0500

    removed the link for flexbox css grid

--- puppyportfolio/css/style.js
+++ puppyportfolio/css/style.js
@@ -1,41 +1,41 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  'body': {
-    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
-    'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
-    'height': [{ 'unit': '%V', 'value': 1 }],
-    'width': [{ 'unit': '%H', 'value': 0.8 }]
-  },
-  'html': {
-    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
-    'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
-    'height': [{ 'unit': '%V', 'value': 1 }],
-    'width': [{ 'unit': '%H', 'value': 0.8 }]
-  },
-  'body': {
-    'fontFamily': ''Lato', sans-serif',
-    'color': '#747704',
-    'fontWeight': 'Regular',
-    'display': 'flex',
-    'flexDirection': 'column'
-  },
-  'text-muted': {
-    'color': '#BCBBBB'
-  },
-  'text-thin': {
-    'fontWeight': '100'
-  },
-  'title-logo': {
-    'marginTop': [{ 'unit': 'px', 'value': 20 }],
-    'width': [{ 'unit': 'px', 'value': 100 }]
-  },
-  'title-super': {
-    'fontSize': [{ 'unit': 'px', 'value': 60 }]
-  },
-  'hr': {
-    'backgroundColor': 'dimgrey !important',
-    'color': 'dimgrey !important',
-    'height': [{ 'unit': 'px', 'value': 5 }, { 'unit': 'string', 'value': '!important' }]
-  }
+    'body': {
+        'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+        'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+        'height': [{ 'unit': '%V', 'value': 1 }],
+        'width': [{ 'unit': '%H', 'value': 0.8 }]
+    },
+    'html': {
+        'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+        'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+        'height': [{ 'unit': '%V', 'value': 1 }],
+        'width': [{ 'unit': '%H', 'value': 0.8 }]
+    },
+    'body': {
+        'fontFamily': 'Lato',
+        'color': '#747704',
+        'fontWeight': 'Regular',
+        'display': 'flex',
+        'flexDirection': 'column'
+    },
+    'text-muted': {
+        'color': '#BCBBBB'
+    },
+    'text-thin': {
+        'fontWeight': '100'
+    },
+    'title-logo': {
+        'marginTop': [{ 'unit': 'px', 'value': 20 }],
+        'width': [{ 'unit': 'px', 'value': 100 }]
+    },
+    'title-super': {
+        'fontSize': [{ 'unit': 'px', 'value': 60 }]
+    },
+    'hr': {
+        'backgroundColor': 'dimgrey !important',
+        'color': 'dimgrey !important',
+        'height': [{ 'unit': 'px', 'value': 5 }, { 'unit': 'string', 'value': '!important' }]
+    }
 });

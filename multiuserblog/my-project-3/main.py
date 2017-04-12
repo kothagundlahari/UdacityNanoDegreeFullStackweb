1182331a15601e835283b96ba2b584abc0964d6c
hari krisha <hari.k.krishna4@gmail.com>
2017-04-12 11:37:21 -0500

    adding validation

--- multiuserblog/my-project-3/main.py
+++ multiuserblog/my-project-3/main.py
@@ -1,18 +1,50 @@
 
 
 import webapp2
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+
+def valid_month(month):
+    if month:
+        cap_month = month.capitalize()
+        if cap_month in months:
+            return cap_month
+def valid_day(day):
+        if day and day.isdigit():
+            day = int(day)
+        if day > 0 and day <= 31:
+            return day
+
+
+def valid_year(year):
+    if year and year.isdigit():
+        year = int(year)
+        if year > 1990 and year < 2020:
+            return year
 
 form = """
 <form method="post">
 What is your Birthday?
+<br>
 <label>Month
-<input type="text" name="Month">
+<input type="text" name="month">
 </label>
 <label>Date
-<input type="text" name="Date">
+<input type="text" name="day">
 </label>
 <label>Year
-<input type="text" name="Year">
+<input type="text" name="year">
 </label>
 
 <br>
@@ -27,10 +59,16 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
        # self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(form)
+
     def post(self):
-         
-        self.response.out.write("Thanks. This is a valid syntax")
+        user_month = valid_month(self.request.get('month'))
+        user_year = valid_year(self.request.get('year'))
+        user_day = valid_day(self.request.get('day'))
+
+        if not(user_month and user_day and user_year):
+            self.response.out.write(form)
+        else:
+            self.response.out.write("Thanks. This is a valid syntax")
 
 
 app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
-

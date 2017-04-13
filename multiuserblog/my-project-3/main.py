e4f4a0bf22200e061a7883591683fcadb8c0d35e
hari krisha <hari.k.krishna4@gmail.com>
2017-04-13 17:08:18 -0500

    working validation

--- multiuserblog/my-project-3/main.py
+++ multiuserblog/my-project-3/main.py
@@ -22,44 +22,44 @@ What is your Birthday?
 
 
 class MainHandler(webapp2.RequestHandler):
-    months = ['January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July',
-          'August',
-          'September',
-          'October',
-          'November',
-          'December']
-def self.valid_month(month):
-    if month:
-        cap_month = month.capitalize()
-        if cap_month in months:
-            return cap_month
-def self.valid_day(day):
+    def valid_month(self,month):
+        months = ['January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December']
+        if month:
+            cap_month = month.capitalize()
+            if cap_month in months:
+                return cap_month
+    def valid_day(self,day):
         if day and day.isdigit():
             day = int(day)
-        if day > 0 and day <= 31:
+        if day>0 and day <= 31:
             return day
-def self.valid_year(year):
-    if year and year.isdigit():
-        year = int(year)
-        if year > 1990 and year < 2020:
-            return year
+    def valid_year(self,year):
+        if year and year.isdigit():
+            year = int(year)
+            if year > 1990 and year < 2020:
+                return year
 
-def get(self):
+    def get(self):
         self.response.write(form)
 
-def post(self):
-        user_month = valid_month(self.request.get('month'))
-        user_year = valid_year(self.request.get('year'))
-        user_day = valid_day(self.request.get('day'))
+    def post(self):
+        user_month = self.valid_month(self.request.get('month'))
+        user_year = self.valid_year(self.request.get('year'))
+        user_day = self.valid_day(self.request.get('day'))
 
         if not(user_month and user_day and user_year):
-            self.response.out.write(form)
+            self.response.out.write("Invalid")
         else:
             self.response.out.write("Thanks. This is a valid syntax")
 

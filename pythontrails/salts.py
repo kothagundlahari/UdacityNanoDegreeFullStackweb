9659dbaf131ec550d03370cc62d2b8c9f1e7e079
hari krisha <hari.k.krishna4@gmail.com>
2017-05-28 14:27:10 -0500

    some thing

--- pythontrails/salts.py
+++ pythontrails/salts.py
@@ -1,12 +1,19 @@
 import random
 import string
+import hashlib
 
-# implement the function make_salt() that returns a string of 5 random
-# letters use python's random module.
-# Note: The string package might be useful here.
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
 
-def make_salt(l):
-    ###Your code here
+# implement the function make_pw_hash(name, pw) that returns a hashed password
+# of the format:
+# HASH(name + pw + salt),salt
+# use sha256
 
-    return ''.join(random.choice(string.lowercase) for i in range(5))
-print make_salt(5)
+def make_pw_hash(name, pw):
+
+    salt = make_salt()
+    h = hashlib.sha256(name+pw+salt).hexdigest()
+    return "%s %s" %(h,salt)
+
+print make_pw_hash('hari', 'hunter')
